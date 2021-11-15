c056a0ee5a1eafd9144d372f3e6f67c6f1e73521
David <davidmacnaugh@gmail.com>
2021-11-15 14:16:18 +1000

    CLAS-59 ASIDE: Movie maker adds 20 frames per image.

--- yo_wrangle/visualise.py
+++ yo_wrangle/visualise.py
@@ -2,16 +2,19 @@ import numpy as np
 import pandas
 from cv2 import cv2
 from pathlib import Path
-from typing import List, Optional
+from sys import platform
+from typing import List, Optional, Tuple
+
+from cv2.cv2 import VideoWriter_fourcc
 
 from yo_wrangle.common import get_all_jpg_recursive, get_id_to_label_map
 
 
 def draw_polygon_on_image(
-    image_file: str,
-    coords: List[List[float]],
-    dst_path: Path = None,
-    class_name: Optional[str] = None,
+        image_file: str,
+        coords: List[List[float]],
+        dst_path: Path = None,
+        class_name: Optional[str] = None,
 ):
     """
     This function takes a copy of an image and draws a bounding box
@@ -42,10 +45,10 @@ def draw_polygon_on_image(
 
 
 def save_bounding_boxes_on_images(
-    images_root: Path,
-    dst_root: Path,
-    ai_file_path: Path,
-    class_list_path: Path,
+        images_root: Path,
+        dst_root: Path,
+        ai_file_path: Path,
+        class_list_path: Path,
 ):
     """
     Save a copy of all images from images_root to dst_root with bounding boxes applied.
@@ -109,11 +112,125 @@ def save_bounding_boxes_on_images(
                 [series["x3"], series["y3"]],
                 [series["x4"], series["y4"]],
             ]
+            dst_path = dst_root / f"{img_path.stem}_{img_path.suffix}"
+            if index == 0:
+                image_file = str(img_path)
+            else:
+                image_file = str(dst_path)
 
-            dst_path = dst_root / f"{img_path.stem}_{index}{img_path.suffix}"
             draw_polygon_on_image(
-                image_file=str(img_path),
+                image_file=image_file,
                 coords=bounding_box_coords,
                 dst_path=dst_path,
                 class_name=class_name
             )
+
+
+def _crop_image_for_given_centre(
+    img: np.ndarray,
+    dim: Tuple[int, int],
+    y_centre: float = 0.5,  # for centre_crop y_centre = 0.5
+):
+    """
+    Returns center cropped image unless the centre_crop parameter is set
+    to False, in which case cropping removes the image foreground.
+
+    Args:
+    img: image to be center cropped
+    dim: dimensions (width, height) to be cropped
+
+    """
+    width, height = img.shape[1], img.shape[0]
+
+    crop_width = dim[0] if dim[0] < img.shape[1] else img.shape[1]
+    crop_height = dim[1] if dim[1] < img.shape[0] else img.shape[0]
+
+    min_x = int(width / 2.0 - crop_width / 2.0)
+    max_x = int(width / 2.0 + crop_width / 2.0)
+
+    min_y = int(height * y_centre - crop_height / 2.0)
+    if min_y < 0:
+        print("min_y is less than 0")
+        min_y = 0
+    max_y = min_y + crop_height
+
+    if max_y > height:
+        print("max_y is greater than image height")
+        max_y = height
+        min_y = max_y - crop_height
+
+    crop_img = img[min_y:max_y, min_x:max_x]
+    return crop_img
+
+
+def _scale_image(img: np.ndarray, factor: float):
+    """Returns resize image by scale factor.
+    This helps to retain resolution ratio while resizing.
+    Args:
+    img: image to be scaled
+    factor: scale factor to resize
+    """
+    return cv2.resize(img, (int(img.shape[1] * factor), int(img.shape[0] * factor)))
+
+
+def zoom_image(
+    zoom_pcnt: float,
+    image: np.ndarray,
+    y_centre: float = 0.5,
+) -> np.ndarray:
+    """
+    Return an image that is 'zoomed': same size as the original provided,
+    but which has undergone a resize and centre crop.
+
+    Usage::
+
+        `zoom_image(zoom_pcnt=90.0, image=image)`
+
+    will return an image for which the features are approximately 10% larger
+    except those features which are near the edge of the image which may
+    be partially removed.
+
+    """
+    width, height = image.shape[1], image.shape[0]
+    factor = 100 / zoom_pcnt
+    image = _scale_image(img=image, factor=factor)
+
+    image = _crop_image_for_given_centre(img=image, dim=(width, height), y_centre=y_centre)
+    return image
+
+
+def make_movie_adding_intermediate_progressive_zooms(
+    img_root: Path,
+    y_centre: float = 0.5,
+):
+    """
+    For each of the images in a directory, 2 additional images that can help
+    create a transition effect when creating a video from the still images.
+
+    Applies resize and centre crop to give a progressive zooming into end of
+    road at the x = 0.5, y = 0.5.  This works great if the horizon of the image
+    is at the centre of the image.
+
+    """
+    done_once = False
+    for img_path in get_all_jpg_recursive(img_root=img_root):
+
+        image = cv2.imread(filename=str(img_path))
+        image_small = _scale_image(img=image, factor=0.3)
+        if not done_once:
+            frame_size = (image_small.shape[1], image_small.shape[0])
+            dst_file = img_root / "an_output_video.mp4"
+            out = cv2.VideoWriter(str(dst_file), VideoWriter_fourcc(*'mp4v'), 52, frame_size)
+            done_once = True
+
+        out.write(image=image_small)
+        for i in range(20):
+            image = zoom_image(zoom_pcnt=99.5, image=image, y_centre=y_centre)
+            image_small = _scale_image(img=image, factor=0.3)
+            out.write(image=image_small)
+
+
+def test_make_avi_movie():
+    make_movie_adding_intermediate_progressive_zooms(
+        img_root=Path("C:\\test", y_centre=0.49)
+    )
