01382ab415d66041f26b8ab8c2df38042042dca0
David MacNaughton <davidmacnaugh@gmail.com>
2021-12-21 21:34:08 +1000

    ASIDE: run_workflows.py example updated.

--- run_workflows.py
+++ run_workflows.py
@@ -1,8 +1,57 @@
 from pathlib import Path
-from yo_wrangle.workflow import run_prepare_dataset_and_train, set_globals
+
+from yo_wrangle.workflow import (
+    run_prepare_dataset_and_train,
+    set_globals,
+    run_find_errors,
+)
 import wrangling_example as dataset_workbook
 
 
 def test_prepare_dataset_and_train():
     set_globals(base_dir=Path(__file__).parent, workbook_ptr=dataset_workbook)
     run_prepare_dataset_and_train()
+
+
+def test_find_errors():
+    set_globals(base_dir=Path(__file__).parent, workbook_ptr=dataset_workbook)
+    run_find_errors(
+        tag="mistakenness",
+        label_filter="WS",
+        limit=64,
+    )
+
+
+def test_export():
+    import fiftyone as fo
+
+    dataset = fo.load_dataset("v9b")
+    dataset.export(
+        export_dir="./.export",
+        dataset_type=fo.types.FiftyOneDataset,
+        export_media=False,
+    )
+
+
+def test_import():
+    # from fiftyone.utils.data.exporters import FiftyOneDatasetExporter # FiftyOneDatasetImporter as Importer
+    name = "dsXXX"
+    import fiftyone as fo
+
+    if name in fo.list_datasets():
+        fo.delete_dataset(name=name)
+    else:
+        pass
+    dataset = fo.Dataset.from_dir(
+        dataset_dir="./.export",
+        dataset_type=fo.types.FiftyOneDataset,
+        name=name,
+    )
+    # fo.launch_app(dataset)
+    set_globals(base_dir=Path(__file__).parent, workbook_ptr=dataset_workbook)
+    run_find_errors(
+        tag="mistakenness",
+        label_filter="WS",
+        limit=64,
+        dataset_label=name,
+    )
