dced78a90d5be349e9fdb1cb26c7f12094133db3
David MacNaughton <davidmacnaugh@gmail.com>
2022-01-24 08:26:41 +1000

    CLAS-120 test_ct_classification_errors()

--- run_workflows.py
+++ run_workflows.py
@@ -1,7 +1,10 @@
 import json
+import shutil
 
 import fiftyone as fo
 from pathlib import Path
+
+import numpy
 
 from yo_ratchet.workflow import (
     run_prepare_dataset_and_train,
@@ -9,7 +12,7 @@ from yo_ratchet.workflow import (
     run_find_errors,
 )
 import wrangling_example as dataset_workbook
-from yo_valuate.reference_csv import get_classification_performance
+from yo_valuate.reference_csv import get_classification_performance, get_actual_vs_inferred_df
 
 
 def test_prepare_dataset_and_train():
@@ -77,12 +80,14 @@ def test_performance_metrics_for_charters_towers():
         )
     csv_group_filters = {
         "Cracking": ["Cracking"],
-        "Risk Defects": ["POTHOLE", "pothole", "Edge", "Potholes"],
+        "Pothole": ["POTHOLE", "pothole", "Potholes"],
+        "Edge Break": ["Edge"],
         "Stripping": ["Stripping", "Strip", "Failure"]
     }
     yolo_group_filters = {
         "Cracking": [0, 1, 2, 16],
-        "Risk Defects": [3, 4],
+        "Pothole": [3],
+        "Edge Break": [4],
         "Stripping": [17, 12],  # Scf = 12; Stp = 17.
     }
     with open("classes.json", "r") as f_in:
@@ -99,3 +104,62 @@ def test_performance_metrics_for_charters_towers():
         classifications_key="Final_Remedy",
         print_table=True,
     )
+
+
+def test_ct_classification_errors():
+    dst_folder_fp = Path("/home/david/RACAS/CT_Errors/fp")
+    dst_folder_fn = Path("/home/david/RACAS/CT_Errors/fn")
+    images_root = Path("/home/david/RACAS/640_x_640/RACAS_CTRC_2021_sealed")
+    truths_csv = Path("/home/david/RACAS/sealed_roads_dataset/CTRC_all_sealed.csv")
+    inferences_path = Path(
+            "/home/david/addn_repos/yolov5/runs/detect/Charters_Towers__srd16.3_conf7pcnt/labels"
+        )
+    csv_group_filters = {
+        "Cracking": ["Cracking"],
+        "Pothole": ["POTHOLE", "pothole", "Potholes"],
+        "Edge Break": ["Edge"],
+        "Stripping": ["Stripping", "Strip", "Failure"]
+    }
+    yolo_group_filters = {
+        "Cracking": [0, 1, 2, 16],
+        "Pothole": [3],
+        "Edge Break": [4],
+        "Stripping": [17, 12],  # Scf = 12; Stp = 17.
+    }
+    with open("classes.json", "r") as f_in:
+        classes_info = json.load(f_in)
+    print()
+    keys = list(csv_group_filters.keys())
+    for key in keys:
+        (dst_folder_fp / key).mkdir(parents=True)
+        (dst_folder_fn / key).mkdir(parents=True)
+    df = get_actual_vs_inferred_df(
+        images_root=images_root,
+        truths_csv=truths_csv,
+        root_inferred_bounding_boxes=inferences_path,
+        csv_group_filters=csv_group_filters,
+        yolo_group_filters=yolo_group_filters,
+        classes_info=classes_info,
+        image_key="Photo_Name",
+        classifications_key="Final_Remedy",
+    )
+    for row in df.iterrows():
+        actual = row[1]["actual_classifications"]
+        inferred = row[1]["inferred_classifications"]
+        comp = numpy.logical_xor(actual, inferred)
+        # ace = comp.nonzero()
+        image_name = str(row[0])
+        false_negative = numpy.logical_and(comp, actual).nonzero()[0]
+        if len(false_negative) > 0:
+            for el in false_negative:
+                class_name = keys[el]
+                shutil.copy(src=str(images_root / image_name), dst=str(dst_folder_fn / class_name / image_name))
+        false_positive = numpy.logical_and(comp, inferred).nonzero()[0]
+        if len(false_positive) > 0:  # There is a disagreement either False Positive or False Negative
+            for el in false_positive:
+                class_name = keys[el]
+                shutil.copy(src=str(images_root / image_name), dst=str(dst_folder_fp / class_name / image_name))
+        # if not numpy.array_equal(row[1]["actual_classifications"], row[1]["inferred_classifications"]):
+        #     print(ace)
+        # else:
+        #     print(ace)
